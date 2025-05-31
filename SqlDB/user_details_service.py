1a2d11c3d6d45e41f6188f92156c5f928841a63d
Krzysztof Kokot <krzysiukokot@interia.pl>
2025-05-31 14:55:33 +0200

    create user details service

---
+++ SqlDB/user_details_service.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Session
+from .models import UserDetails
+
+
+def create_details(db: Session, user_id: str, weight: float, year_of_birth: int, gender: str, allergies: str) -> UserDetails:
+    user_details = UserDetails(user_id=user_id, weight=weight,
+                               year_of_birth=year_of_birth, gender=gender, allergies=allergies)
+    db.add(user_details)
+    db.commit()
+    db.refresh(user_details)
+    return user_details
+
+
+def get_user_details_by_user_id(db: Session, user_id: str) -> UserDetails | None:
+    return db.query(UserDetails).filter(UserDetails.user_id == user_id).first()
+
+
+def is_user_details_exists(db: Session, user_id: str) -> bool:
+    return db.query(UserDetails).filter(UserDetails.user_id == user_id).first() is not None
